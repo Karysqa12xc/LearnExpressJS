d75717be80cdea87f960a33d1c01caa1ffdc3a7e
Karysqa <85331340+Karysqa12xc@users.noreply.github.com>
2024-04-22 03:54:34 +0700

    feat: add header, changer file handlebars to hbs

--- src/index.js
+++ src/index.js
@@ -5,18 +5,33 @@ const morgan = require("morgan");
 const app = express();
 const port = 3000;
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded());
 app.use(morgan("combined"));
-app.engine('hbs', handlebars.engine({
+app.engine(
+  "hbs",
+  handlebars.engine({
     extname: ".hbs",
-}));
-app.set('view engine', 'hbs');
+  })
+);
+app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "resources\\views"));
 app.get("/", (req, res) => {
   res.render("home");
 });
 app.get("/news", (req, res) => {
-    res.render("news");
-  });
+  console.log(req.query);
+  res.render("news");
+});
+app.get("/link", (req, res) => {
+  res.render("link");
+});
+app.get("/search", (req, res) => {
+  res.render("search");
+});
+app.post("/search", (req, res) => {
+  console.log(req.body.gender);
+  res.send("");
+});
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
